cd6fcfdaf44f451d964f8cd86fd5e28b5e2eca9d
陈宇仑 <308813800@qq.com>
2017-09-11 14:13:07 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1505109923327_1955';
+
+  // add your config here
+
+  return config;
+};
